37e91a7b5cd57f9244c580e0e0be26811e7c70f6
Heitor Murara <heitormurara@gmail.com>
2020-06-14 19:31:26 -0300

    Display Rick and Morty characters into ListView

--- lib/pages/characters.dart
+++ lib/pages/characters.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_listview/models/rick_morty.dart';
+import 'package:flutter_listview/repos/rick_morty_repo.dart';
+import 'package:flutter_listview/widgets/character_card.dart';
 
 class CharactersPage extends StatefulWidget {
   @override
@@ -6,13 +9,44 @@ class CharactersPage extends StatefulWidget {
 }
 
 class _CharactersPageState extends State<CharactersPage> {
+
+  List<Character> characters = [];
+  int page = 0;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() {
+    setState(() {
+      isLoading = true;
+    });
+    RickAndMortyRepo().getCharacters(page: page + 1).then((res) {
+      page++;
+      setState(() {
+        isLoading = false;
+        characters.addAll(res.results);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Rick and Morty"),
       ),
-      body: Container(),
+      body: isLoading 
+      ? Center(child: CircularProgressIndicator(),) 
+      : ListView.builder(
+          itemCount: characters.length,
+          itemBuilder: (BuildContext context, int index) {
+            return CharacterCard(character: characters[index],);
+          }
+        ),
     );
   }
 }
