b842c6880e25052a21cbe1806dd5cd7cbac43d11
Heitor Murara <heitormurara@gmail.com>
2020-06-14 17:12:33 -0300

    Refactor into Basic ListView

--- lib/main.dart
+++ lib/main.dart
@@ -23,44 +23,26 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('ListView Demo'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              "5 tips to get the best out of your holidays",
-              style: Theme.of(context).textTheme.display1,
-            ),
-            SizedBox(height: 24),
-            Text(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-              style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Text(
-                "1. Subscribe to raywenderlich.com",
-                style: TextStyle(fontSize: 22),
-              ),
-            ),
-            Text(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-              style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Text(
-                "2. Plan ahead",
-                style: TextStyle(fontSize: 22),
-              ),
-            ),
-            Text(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-              style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20),
-            ),
-          ],
-        ),
-      ),
+      body: ListView(
+        padding: EdgeInsets.all(8),
+        children: <Widget> [
+          ListTile(
+            title: Text('Account'),
+            leading: Icon(Icons.person),
+            trailing: Icon(Icons.keyboard_arrow_right),
+          ),
+          ListTile(
+            title: Text('Display'),
+            leading: Icon(Icons.screen_lock_portrait),
+            trailing: Icon(Icons.keyboard_arrow_right),
+          ),
+          ListTile(
+            title: Text('Apps'),
+            leading: Icon(Icons.apps),
+            trailing: Icon(Icons.keyboard_arrow_right),
+          ),
+        ],
+      )
     );
   }
 }
